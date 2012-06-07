e61bc97ee8d53a7136ee96a384bb640a409afe4d
Stefan Strigler <zeank@v3.fund.strigler.de>
2012-06-07 23:24:01 +0200

    allow port to be configurable

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.get('/', function(req, res){
     }
 });
 
-app.listen(3000, function(){
+app.listen(config.port, function(){
     console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
 
