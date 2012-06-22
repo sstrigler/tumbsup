39f07a0269228d4909418ca7847ee155ce4d134f
Stefan Strigler <stefan@strigler.de>
2012-06-22 12:54:03 +0200

    show share buttons on login page as well

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.get('/', function(req, res){
                                  avatar: req.session.auth.tumblr.user['avatar-url']});
         });
     } else {
-        res.render('login', { title: 'tumblikes' });
+        res.render('login', { title: 'tumblikes', host: config.host });
     }
 });
 
