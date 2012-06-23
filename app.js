2dfcef44286fbd1a1423a76a2b1d351296c80deb
Stefan Strigler <stefan@strigler.de>
2012-06-23 18:05:14 +0200

    make use of express.favicon()

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.configure(function(){
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.logger());
+    app.use(express.favicon());
     app.use(express.bodyParser());
     app.use(express.cookieParser());
     app.use(express.session({
